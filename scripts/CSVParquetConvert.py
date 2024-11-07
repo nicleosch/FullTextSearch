557733a2aa83dd8da6d3efd3ab69764c38881dc4
Julian-JP <julian.laux@tum.de>
2024-11-07 16:10:09 +0100

    Update datatype to binaryarray in conversion script

--- scripts/CSVParquetConvert.py
+++ scripts/CSVParquetConvert.py
@@ -8,17 +8,19 @@ parser = argparse.ArgumentParser(description="Convert CSV to Parquet")
 requiredArgs = parser.add_argument_group('required arguments')
 requiredArgs.add_argument("-i", "--input", help="Input path to a CSV file", required=True)
 requiredArgs.add_argument("-o", "--output", help="Path to the output parquet file", required=True)
-requiredArgs.add_argument("-t", "--title", help="Column-name of the title column", required=True)
-requiredArgs.add_argument("-c", "--content", help="Column-name of the text column", required=True)
+requiredArgs.add_argument("-n", "--name", help="Column-name of the text column", required=True)
 
 args = vars(parser.parse_args())
 
 try:
+
     df = pd.read_csv(args["input"], header=0)
-    metadata = { b'creator': b'CSVParquetConvert'}
-    df_filtered = df[[args["title"], args["content"]]].rename(columns={args["title"]: "ID", args["content"]: "Content"})
+    df_filtered = df[[args["name"]]].rename(columns={args["name"]: "text"})
 
     print(df_filtered)
+
+    metadata = { b'creator': b'CSVParquetConvert'}
+    schema = pa.schema([("text", pa.binary())])
 
     table = pa.Table.from_pandas(df_filtered, preserve_index=False)
     table = table.replace_schema_metadata(metadata)
